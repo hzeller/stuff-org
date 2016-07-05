fa9fb6feac646263a13ce3e461ee9fedfb5d573b
Henner Zeller <h.zeller@acm.org>
2016-07-04 21:57:42 -0700

    o Simple AND and OR boolean logic in search terms, including   AND operator precedence and way to add  parenthesis.
    o Provide query rewrite in case someone adds 'Ohm' to a number. We
      don't store the 'Ohm' string in the value of resistors, so it wouldn't
      find anything. So rewrite it to search for just the number in resistor
      category, but also the original value the user typed in case that
      is actually occuring somewhere.
      So roughly
         10kOhm  ->  (10kOhm | (10k (resistor|potentiometer)))
      Now, the boolean terms from above are really handy.

--- stuff/search.go
+++ stuff/search.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"sort"
 	"strings"
 	"sync"
@@ -10,7 +11,23 @@ func isSeparator(c byte) bool {
 	return c == ' ' || c == '\t' || c == '\n' || c == '.' || c == ',' || c == ';'
 }
 
+func queryRewrite(term string) string {
+	and_rewrite_, _ := regexp.Compile(`(?i)( and )`)
+	term = and_rewrite_.ReplaceAllString(term, " ")
+
+	or_rewrite_, _ := regexp.Compile(`(?i)( or )`)
+	term = or_rewrite_.ReplaceAllString(term, " | ")
+
+	possible_resistor, _ := regexp.Compile(`(?i)([0-9]+[kM]?)(\s*Ohm?)`)
+	term = possible_resistor.ReplaceAllString(term, "($0 | ($1 (resistor|potentiometer|r-network)))")
+	return term
+}
+
 func preprocessTerm(term string) string {
+	// For simplistic parsing, add spaces around special characters (|)
+	logical_term, _ := regexp.Compile(`(?i)([\(\)\|])`)
+	term = logical_term.ReplaceAllString(term, " $1 ")
+
 	// * Lowercase: we want to be case insensitive
 	// * Dash remove: we consider dashes to join words and we want to be
 	//   agnostic to various spellings (might break down with minus signs
@@ -49,10 +66,31 @@ func maxlist(values ...float32) (max float32) {
 	return
 }
 
-// Matches the component and returns a score
-func (c *SearchComponent) MatchScore(term string) float32 {
-	var total_score float32 = 0.0
-	for _, part := range strings.Split(term, " ") {
+// Score terms, starting at index 'start', returns index it went up to.
+// Treats terms as 'AND' until it reaches an 'OR' (|) operator.
+func (c *SearchComponent) scoreTerms(terms []string, start int) (float32, int) {
+	var last_or_term float32 = 0.0
+	var current_score float32 = 0.0
+	for i := start; i < len(terms); i++ {
+		part := terms[i]
+		if part == "(" && i < len(terms)-1 {
+			sub_score, subterm_end := c.scoreTerms(terms, i+1)
+			if sub_score <= 0 {
+				current_score = -1000 // See below for reasoning
+			} else {
+				current_score += sub_score
+			}
+			i = subterm_end
+			continue
+		}
+		if part == "|" {
+			last_or_term = maxlist(last_or_term, current_score)
+			current_score = 0
+			continue
+		}
+		if part == ")" && start != 0 {
+			return maxlist(last_or_term, current_score), i
+		}
 		// Avoid keyword stuffing by looking only at the field
 		// that scores the most.
 		// NOTE: more fields here, add to lowerCased below.
@@ -61,14 +99,25 @@ func (c *SearchComponent) MatchScore(term string) float32 {
 			1.5*StringScore(part, c.preprocessed.Description),
 			1.2*StringScore(part, c.preprocessed.Notes),
 			1.0*StringScore(part, c.preprocessed.Footprint))
-
 		if score == 0 {
-			return 0 // all words must match somehow
+			// We essentially would do an early out here, but
+			// since we're in the middle of parsing until we reach
+			// the next OR, we do the simplistic thing here:
+			// just make it impossible to have max()
+			// give a positive result with the last term.
+			// (todo: if this becomes a problem, implement early out)
+			current_score = -1000
 		} else {
-			total_score += score
+			current_score += score
 		}
 	}
-	return total_score
+	return maxlist(last_or_term, current_score), len(terms)
+}
+
+// Matches the component and returns a score
+func (c *SearchComponent) MatchScore(term string) float32 {
+	score, _ := c.scoreTerms(strings.Fields(term), 0)
+	return score
 }
 
 type SearchComponent struct {
@@ -151,6 +200,7 @@ func (s *FulltextSearch) Update(c *Component) {
 	s.lock.Unlock()
 }
 func (s *FulltextSearch) Search(search_term string) []*Component {
+	search_term = queryRewrite(search_term)
 	search_term = preprocessTerm(search_term)
 	s.lock.Lock()
 	scoredlist := make(ScoreList, 0, 10)
---
+++ stuff/search_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"testing"
+)
+
+func expectMatch(t *testing.T, s *SearchComponent, term string, expected bool) {
+	if (s.MatchScore(preprocessTerm(term)) <= 0) == expected {
+		if expected {
+			t.Errorf("'%s' did not match as expected", term)
+		} else {
+			t.Errorf("'%s' unexpectedly matched", term)
+		}
+	}
+}
+
+func expectEqual(t *testing.T, a string, b string) {
+	if a != b {
+		t.Errorf("'%s' != '%s'", a, b)
+	}
+}
+
+func TestSearchOperators(t *testing.T) {
+	s := &SearchComponent{
+		preprocessed: &Component{
+			Category: "resist",
+			Value:    "foo",
+		},
+	}
+	expectMatch(t, s, "foo", true)  // direct match
+	expectMatch(t, s, "bar", false) // not a match
+
+	// AND expressions require all terms match
+	expectMatch(t, s, "foo foo", true)
+	expectMatch(t, s, "foo bar", false)
+	expectMatch(t, s, "foo bar", false)
+
+	// Simple OR expression. One term matches
+	expectMatch(t, s, "foo|bar", true)   // OR expression
+	expectMatch(t, s, "(foo|bar)", true) // same thing
+	expectMatch(t, s, "(foo|bar", true)  // unbalanced paren: ok
+	expectMatch(t, s, "foo|bar)", true)  // unbalanced paren: ok
+
+	expectMatch(t, s, "bar|baz", false) // OR expression, no matches
+
+	// AND of ORs, but not all AND terms matching
+	expectMatch(t, s, "(foo|bar) (bar|baz)", false)
+	expectMatch(t, s, "(bar|baz) (foo|baz)", false)
+	expectMatch(t, s, "(foo|baz) (foo|baz)", true) // both AND terms ok
+
+	// OR together with AND
+	expectMatch(t, s, "foo (foo|bar)", true)
+	expectMatch(t, s, "(foo|bar) foo", true)
+	expectMatch(t, s, "baz (foo|bar)", false) // AND-ing with non-match
+	expectMatch(t, s, "(foo|bar) baz", false)
+	expectMatch(t, s, "((foo|bar) baz)", false)
+
+	// Simulating the Ohm-rewrite
+	expectMatch(t, s, "(bar | (foo (baz|resist)))", true)
+	expectMatch(t, s, "(bar | (foo (baz|wrongcategory)))", false)
+	expectMatch(t, s, "(foo | (bar (baz|resist)))", true)
+	expectMatch(t, s, "(foo foo | (bar (baz|resist)))", true)
+	expectMatch(t, s, "(bar | (bar (baz|resist)))", false)
+}
+
+func TestQueryRewrite(t *testing.T) {
+	// Identity
+	expectEqual(t, queryRewrite("foo"), "foo")
+	expectEqual(t, queryRewrite("10k"), "10k")
+
+	// AND, OR rewrite
+	expectEqual(t, queryRewrite("foo AND bar"), "foo bar")
+	expectEqual(t, queryRewrite("foo OR bar"), "foo | bar")
+	expectEqual(t, queryRewrite("(foo AND bar) OR (bar AND baz)"),
+		"(foo bar) | (bar baz)")
+
+	// Only mess with it if it is with spaces.
+	expectEqual(t, queryRewrite("fooANDbar"), "fooANDbar")
+	expectEqual(t, queryRewrite("fooORbar"), "fooORbar")
+
+	// We store resistors without the 'Ohm' suffix. So if someone adds
+	// Ohm to the value, expand the query to match the raw number plus
+	// something that narrows it to resistor. But also still look for the
+	// original value in case this is something
+	expectEqual(t, queryRewrite("10k"), "10k") // no rewrite
+	expectEqual(t, queryRewrite("10kOhm"), "(10kOhm | (10k (resistor|potentiometer|r-network)))")
+	expectEqual(t, queryRewrite("10k Ohm"), "(10k Ohm | (10k (resistor|potentiometer|r-network)))")
+}
