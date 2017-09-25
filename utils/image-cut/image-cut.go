0a9a2eec6172a3b600a1ec9164615947e18fb66e
Henner Zeller <h.zeller@acm.org>
2017-09-24 22:36:44 -0700

    Image cut improvements.
    o Make image divisions configurable.
    o Provide a percentage of cropped edges on images.

--- utils/image-cut/image-cut.go
+++ utils/image-cut/image-cut.go
@@ -11,11 +11,6 @@ import (
 	"os"
 )
 
-const (
-	XDivisions = 8
-	YDivisions = 5
-)
-
 type SubImage struct {
 	image image.Image
 	rect  image.Rectangle
@@ -39,13 +34,23 @@ func (s *SubImage) At(x, y int) color.Color {
 }
 
 func main() {
-	box_id := flag.String("container", "", "Name of the container image.")
+	box_id := flag.String("container", "", "Basename of the container image, e.g. 'A' for 'A.jpg'")
+	xdivs := flag.Int("xdiv", 8, "Divisions in X direction")
+	ydivs := flag.Int("ydiv", 5, "Divisions in Y direction")
+	border_percent := flag.Int("border_percent", 5,
+		"Percent of border removed")
+	out_dir := flag.String("out_dir", "out", "Output directory")
+
 	flag.Parse()
 
 	if box_id == nil || *box_id == "" {
-		log.Fatal("Need the --container parameter")
+		log.Fatal("Need the --container parameter. Something like 'A' for 'A.jpg'")
 	}
-	reader, err := os.Open(fmt.Sprintf("container-%s.jpg", *box_id))
+	if *border_percent < 0 || *border_percent > 50 {
+		log.Fatal("--border_percent needs to be less than 50%")
+	}
+
+	reader, err := os.Open(fmt.Sprintf("%s.jpg", *box_id))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -57,15 +62,17 @@ func main() {
 	bounds := m.Bounds()
 	width := bounds.Max.X - bounds.Min.X
 	height := bounds.Max.Y - bounds.Min.Y
-	div_width := width / XDivisions
-	div_height := height / YDivisions
-	log.Printf("size: %dx%d", width, height)
-	for x := 0; x < XDivisions; x++ {
-		for y := 0; y < YDivisions; y++ {
+	div_width := width / *xdivs
+	div_height := height / *ydivs
+	cut_w := div_width * *border_percent / 100
+	cut_h := div_height * *border_percent / 100
+	log.Printf("imagelet size: %dx%d [border: %dx%d]", div_width, div_height, cut_w, cut_h)
+	for x := 0; x < *xdivs; x++ {
+		for y := 0; y < *ydivs; y++ {
 			subimage := NewSubImage(m,
-				image.Rect(x*div_width, y*div_height,
-					(x+1)*div_width, (y+1)*div_height))
-			name := fmt.Sprintf("out/%s:%c%d.jpg", *box_id, y+'A', x+1)
+				image.Rect(x*div_width+cut_w, y*div_height+cut_h,
+					(x+1)*div_width-cut_w, (y+1)*div_height-cut_h))
+			name := fmt.Sprintf("%s/%s:%c%d.jpg", *out_dir, *box_id, y+'A', x+1)
 			log.Printf(name)
 			writer, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 			if err != nil {
